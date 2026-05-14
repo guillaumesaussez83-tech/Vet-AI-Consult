8278a3d324659e10d2d0807b546e6cd52895ae5a
guillaumesaussez83-tech <guillaume.saussez.83@gmail.com>
2026-05-14 15:30:24 +0200

    Update import statements for ClerkProvider and App

--- artifacts/vetcare/src/main.tsx
+++ artifacts/vetcare/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ClerkProvider } from "@clerk/clerk-react";
-import App from "./App";
+import { ClerkProvider } from "@clerk/react";
+import { App } from "./App";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import "./index.css";
 
